071f56c2043e35c465ab4a9e3e4983864481ce13
Alan Quintiliano <alan.etm@gmail.com>
2022-01-05 10:02:22 -0300

    Removing methods

--- app/src/main/java/com/daitan/example/socialnetwork/model/post/PostService.kt
+++ app/src/main/java/com/daitan/example/socialnetwork/model/post/PostService.kt
@@ -1,13 +1,5 @@
 package com.daitan.example.socialnetwork.model.post
 
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-
 interface PostService {
-    @POST("post")
-    suspend fun insertPost(@Body post: Post): Post
 
-    @GET("post")
-    suspend fun getPosts(): List<Post>
 }
